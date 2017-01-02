a14c8326dda59532c8d3a6abaf360e6380c5b1d4
user <user@DESKTOP-32S7LSP>
2017-01-02 15:46:17 +0900

    main 함수ㅜ 오류 수정 변수 s 추가

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -2,6 +2,7 @@ package helloworld;
 
 public class Helloworld {
 	public static void main(String[] args) {
-	 System.out.println("Helloworld");
+	String s= "HelloWorld";
+	 System.out.println(s);
 	}
 }
